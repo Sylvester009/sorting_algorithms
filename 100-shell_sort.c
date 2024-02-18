120c0590bb6de03835ecb2e405804067428daec0
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-18 19:33:24 +0100

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,30 +9,27 @@
 
 void shell_sort(int *array, size_t size)
 {
-    if (!array || size < 2)
+    int index, j_index, space, num, K_max, temp;
+
+    while (!array || size < 2)
         return;
 
-    int num = (int)size;
-    int space, index, j_index, temp;
+    num = (int)size;
+    K_max = 1; // Initialize K_max
 
-    for (space = 1; space < num; space = (space * 3) + 1);
-
-    while (space > 0)
+    for (space = K_max; space > 0; space = (space - 1) / 3)
     {
-        space = (space - 1) / 3;
-
         for (index = space; index < num; index++)
         {
             temp = array[index];
-
             for (j_index = index; j_index >= space && array[j_index - space] > temp; j_index -= space)
             {
                 array[j_index] = array[j_index - space];
             }
-
             array[j_index] = temp;
         }
 
+        K_max = space;
         print_array(array, size);
     }
 }
