229fa282cb5fd9edfb67cb6fa7cb75031156c416
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-18 16:21:50 +0100

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,45 +1,40 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts doubly_linked list of int type in an ascending order
- * using the insertion of sorting  algorithm.
+ * insertion_sort_list - Sorts doubly_linked list of int type
+ * in an ascending order using the insertion of sorting  algorithm.
  * @list: it Points to the head of the doubly_linked list.
  */
+
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *previous, *next_node, *temp;
-
-    while (!list || !(*list) || !((*list)->next))
-        return;
-
-    current = (*list);
-    next_node = (*list)->next;
-
-    while (next_node)
-    {
-        previous = current;
-        current = next_node;
-        next_node = next_node->next;
-
-        if (previous && current->n < previous->n)
-        {
-            if (previous->prev)
-                previous->prev->next = current;
-            else
-                *list = current;
-
-            while (current->next)
-                current->next->prev = previous;
-
-            temp = current->next;
-            current->next = previous;
-            current->prev = previous->prev;
-            previous->next = temp;
-            previous->prev = current;
-
-            print_list(*list);
-
-            previous = current->prev;
-        }
-    }
+	listint_t *current, *follow, *new, *temp;
+ *previous, *new, *temp;
+	while (!list || !(*list) || !((*list)->next))
+		return;
+	previous = (*list);
+	current = (*list)->next;
+	while (current)
+	{
+		new = current->next;
+		while (previous && current->n < previous->n)
+		{
+			if (previous->prev)
+				previous->prev->next = current;
+			else
+				*list = current;
+			while (current->next)
+				current->next->prev = previous;
+			temp = current->next;
+			current->next = previous;
+			current->prev = previous->prev;
+			previous->next = temp;
+			previous->prev = current;
+			print_list(*list);
+			previous = current->prev;
+		}
+		current = new;
+		while (current)
+			previous = current->prev;
+	}
 }
