07498bc4a200a6563eafec0f981d2cc0687173b7
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-18 17:49:06 +0100

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,47 +1,49 @@
 #include "sort.h"
 
 /**
- * partition - scans a partition of an array of integers for values less than
+ * partitionArray - scans a partition of an array of integers for values less than
  * pivot value, and swaps them with first value in partition, then swaps pivot
  * value with first value in partition
  * @array: array of integers to be sorted
- * @low: index in array that begins partition
- * @high: index in array that ends partition
+ * @left: index in array that begins partition
+ * @right: index in array that ends partition
  * @size: amount of elements in array
  * Return: new index at which to start new recursive partition
  */
-int partition(int *array, int low, int high, size_t size)
+int partitionArray(int *array, int left, int right, size_t size)
 {
-	int i, j, pivot, temp;
+int index, j_index, piv, temp;
+piv = array[right];
+index = left;
+j_index = left;
+while (j_index < right)
+{
+if (array[j_index] < piv)
+{
+temp = array[index];
+array[index] = array[j_index];
+array[j_index] = temp;
+if (array[index] != array[j_index])
+print_array(array, size);
+index++;
+}
+j_index++;
+}
 
-	pivot = array[high];
-	i = low;
-	for (j = low; j < high; j++)
-	{
-		if (array[j] < pivot)
-		{
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-			if (array[i] != array[j])
-				print_array(array, size);
-			i++;
-		}
-	}
-	temp = array[i];
-	array[i] = array[high];
-	array[high] = temp;
-	if (array[i] != array[high])
-		print_array(array, size);
-	return (i);
+temp = array[index];
+array[index] = array[right];
+array[right] = temp;
+if (array[index] != array[right])
+print_array(array, size);
+return (index);
 }
 
 /**
  * quicksort - recursively sorts array of integers by separating into two
  * partitions, using Lomuto quick sort
  * @array: array of integers to be sorted
- * @low: index in array that begins partition
- * @high: index in array that ends partition
+ * @left: index in array that begins partition
+ * @right: index in array that ends partition
  * @size: amount of elements in array
  */
 void quicksort(int *array, int low, int high, size_t size)
