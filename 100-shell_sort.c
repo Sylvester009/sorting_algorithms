dc5ceedcbfbda194302eaf6f0955cf30d881133b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-18 18:52:24 +0100

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -6,29 +6,37 @@
  * @array: array of integers to be sorted
  * @size: amount of elements in array
  */
-void shell_sort(int *array, size_t size) {
-    int index, j_index, space, num, K_max, temp;
+void shell_sort(int *array, size_t size)
+{
+	int i, j, gap, n, Knuth_max, temp;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    num = (int)size;
-    for (space = 1; space < num; space = (space * 3) + 1) {
-        K_max = space;
-    }
-
-    for (space = K_max; space > 0; space = (space - 1) / 3) {
-        for (index = space; index < num; index++) {
-            temp = array[index];
-            for (j_index = index; j_index >= space && array[j_index - space] > temp; j_index -= space) {
-                array[j_index] = array[j_index - space];
-            }
-            array[j_index] = temp;
-        }
-        // Print array after each iteration
-        for (int i = 0; i < size; i++) {
-            std::cout << array[i] << " ";
-        }
-        std::cout << std::endl;
-    }
+	n = (int)size;
+	for (gap = 1; gap < n; gap = (gap * 3) + 1)
+	{
+		Knuth_max = gap;
+	}
+/* Start with the largest Knuth seq value less than n as gap, */
+/* and work down sequence to a gap of 1 */
+	for (gap = Knuth_max; gap > 0; gap = (gap - 1) / 3)
+	{
+		/* Do a gapped insertion sort for this gap size. */
+		for (i = gap; i < n; i++)
+		{
+			/* add array[i] to gap sorted elements; */
+			/* save array[i] in temp in preparation to overwrite */
+			temp = array[i];
+			/* shift earlier gap-sorted elements up until the */
+			/* correct location for array[i] is found */
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+/* temp original array[i]) to its correct location */
+			array[j] = temp;
+		}
+		print_array(array, size);
+	}
 }
