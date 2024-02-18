bac579c607934d91a44d421c86040000ba77c016
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-18 17:19:31 +0100

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,28 +7,33 @@
  * @size: amount of elements in array
  */
 void selection_sort(int *array, size_t size)
- {
-    if (!array || size < 2)
-        return;
+{
+	int index, j_index, min_j_dex, temp, num = (int)size;
 
-    int currentIndex, scanIndex, minIndex, temp, n = static_cast<int>(size);
+	if (!array || size < 2)
+		return;
 
-    // At every position in the n-member array
-    for (currentIndex = 0; currentIndex < n - 1; currentIndex++) {
-        // Scan from that position to the end
-        minIndex = currentIndex;
-        for (scanIndex = currentIndex + 1; scanIndex < n; scanIndex++) {
-            // Determine the minimum value in that range
-            if (array[scanIndex] < array[minIndex]) {
-                minIndex = scanIndex;
-            }
-        }
-        // If the minimum value is lower than the value at the start of the range, swap them
-        if (minIndex != currentIndex) {
-            temp = array[currentIndex];
-            array[currentIndex] = array[minIndex];
-            array[minIndex] = temp;
-            print_array(array, size);
-        }
-    }
+	/* at every position in the n-member array */
+	for (index = 0; index < num - 1; index++)
+	{
+		/* scan from that position to the end */
+		min_j_dex = index;
+		for (j_index = index + 1; j_index < num; j_index++)
+		{
+			/* determine the minimum value in that range */
+			if (array[j_index] < array[min_j_dex])
+			{
+				min_j_dex = j_index;
+			}
+		}
+		/* and if it is lower than the value at start of range, */
+		/* swap them */
+		if (min_j_dex != index)
+		{
+			temp = array[index];
+			array[index] = array[min_j_dex];
+			array[min_j_dex] = temp;
+			print_array(array, size);
+		}
+	}
 }
