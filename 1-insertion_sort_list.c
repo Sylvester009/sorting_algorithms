eebe3945103218737b418244ad5167b92ee8db92
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-18 16:28:49 +0100

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,7 +8,7 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *previous, *next_node, *temp;
+    listint_t *current, *previous, *new, *temp;
 
     if (!list || !(*list) || !((*list)->next))
         return;
