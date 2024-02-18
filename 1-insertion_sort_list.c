c1e79f55662e9d8db9185f40d321bc1c6d76bbbd
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-18 16:09:50 +0100

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,34 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending
- * order using an insertion sort algorithm
- * @list: doubly linked list of integers to be sorted
+ * insertion_sort_list - Sorts doubly_linked list of int type in an ascending order
+ * using the insertion of sorting  algorithm.
+ * @list: it Points to the head of the doubly_linked list.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *lead, *follow, *new, *temp;
+listint_t *current, *sorted, *temp;
+if (!list || !(*list) || !((*list)->next))
+return;
+/* Initialize the sorted sublist with the first node */
+sorted = (*list)->next;
+if (sorted)
+{
+current = sorted;
+sorted = sorted->next;
+while (current->prev && current->n < current->prev->n)
+{
+temp = current->prev;
+current->prev = temp->prev;
+temp->next = current->next;
+current->next = temp;
+temp->prev = current;
+while  (temp->next)
+temp->next->prev = temp;
 
-	if (!list || !(*list) || !((*list)->next))
-		return;
-
-	/* dance begins with 1st from house left following */
-	follow = (*list);
-	/* and next dancer to house right leading */
-	lead = (*list)->next;
-	while (lead)
-	{
-		new = lead->next;
-		while (follow && lead->n < follow->n)
-		{
-			/* lead and follow swap positions */
-			if (follow->prev)
-				follow->prev->next = lead;
-			else
-				/* if lead makes it to house left, now head */
-				*list = lead;
-			if (lead->next)
-				lead->next->prev = follow;
-			temp = lead->next;
-			lead->next = follow;
-			lead->prev = follow->prev;
-			follow->next = temp;
-			follow->prev = lead;
-			print_list(*list);
-			/* compare next pair, flowing to house left */
-			follow = lead->prev;
-		}
-		/* lead sorted to left, new cycle starts @ right leading edge */
-		lead = new;
-		if (lead)
-			follow = lead->prev;
-	}
+while (!current->prev)
+*list = current;
+print_list(*list);
+}
+}
 }
