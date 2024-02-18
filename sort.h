77b93958c46cf33ea7d053d1dcdc81c1899e77f5
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-18 17:47:46 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -28,18 +28,16 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* supplied functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* mandatory tasks */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
 /* quick_sort() helpers */
-int partition(int *array, int low, int high, size_t size);
+int partitionArray(int *array, int left, int right, size_t size);
 void quicksort(int *array, int low, int high, size_t size);
 
 /* advanced tasks */
