6a6cbee4246090ee44b48a3de91e3e958f2e1891
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-18 17:30:04 +0100

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,43 +1,42 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array of integers using a selction sort
- * algorithm
- * @array: array of integers to be sorted
- * @size: amount of elements in array
+ * selection_sort - it sorts arrays of int type using
+ * a selctions of algorithm
+ * @array: it is the  array of int to be sorted
+ * @size: the amount of elements in an array
  */
 void selection_sort(int *array, size_t size)
 {
-    int currentIndex = 0, jIndex, minIndex, temp, num = (int)size;
+int currentIndex = 0, jIndex, minIndex, temp, num = (int)size;
 
-    if (!array || size < 2)
-        return;
+if (!array || size < 2)
+return;
 
-    /* Iterate until the current index reaches the end of the array */
-    while (currentIndex < num - 1)
-    {
-        minIndex = currentIndex;
-        jIndex = currentIndex + 1;
+/* Iterate until the current index reaches the end of the array */
+while (currentIndex < num - 1)
+{
+minIndex = currentIndex;
+jIndex = currentIndex + 1;
 
-        /* Find the index of the minimum value in the remaining unsorted part of the array */
-        while (jIndex < num)
-        {
-            if (array[jIndex] < array[minIndex])
-            {
-                minIndex = jIndex;
-            }
-            jIndex++;
-        }
+/* Find the index of the min val in the  unsorted part of the array */
+while (jIndex < num)
+{
+if (array[jIndex] < array[minIndex])
+{
+minIndex = jIndex;
+}
+jIndex++;
+}
+/* Swap the current element with the min element if they're not the same */
+if (minIndex != currentIndex)
+{
+temp = array[currentIndex];
+array[currentIndex] = array[minIndex];
+array[minIndex] = temp;
+print_array(array, size);
+}
 
-        /* Swap the current element with the minimum element if they are not the same */
-        if (minIndex != currentIndex)
-        {
-            temp = array[currentIndex];
-            array[currentIndex] = array[minIndex];
-            array[minIndex] = temp;
-            print_array(array, size);
-        }
-
-        currentIndex++;
-    }
+currentIndex++;
+}
 }
