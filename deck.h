07deb30245ba1989a5848852f96951a3d430053c
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-19 16:57:31 +0100

    Update deck.h

--- deck.h
+++ deck.h
@@ -39,10 +39,9 @@ typedef struct deck_node_s
 
 
 void sort_deck(deck_node_t **deck);
-int _strcmp(const char *s1, const char *s2);
-char get_value(deck_node_t *card);
-void insertion_sort_deck_kind(deck_node_t **deck);
-void insertion_sort_deck_value(deck_node_t **deck);
-void sort_deck(deck_node_t **deck);
+int _strcmp(const char *str1, const char *str2);
+char gettervalues(deck_node_t *point);
+void insertion_by_sorting(deck_node_t **head);
+void sorting_value(deck_node_t **head);
 
 #endif /* DECK_H */
