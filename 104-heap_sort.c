f2fe2b5acca397f5f39e1f301bce86c1cac2f1bd
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-19 09:01:30 +0100

    Update 104-heap_sort.c

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -8,23 +8,23 @@
  */
 void ConvertToHeap(int *array, size_t size, size_t end, size_t start)
 {
-	size_t left, right, large;
+size_t left, right, large;
 
-	left = 2 * start + 1;
-	right = 2 * start + 2;
-	large = start;
+left = 2 * start + 1;
+right = 2 * start + 2;
+large = start;
 
-	if (left < end && array[left] > array[large])
+if (left < end && array[left] > array[large])
 		large = left;
-	if (right < end && array[right] > array[large])
+if (right < end && array[right] > array[large])
 		large = right;
 
-	while (large != start)
-	{
-		ints_swapping(array + start, array + large);
-		print_array(array, size);
-		ConvertToHeap(array, size, end, large);
-	}
+if (large != start)
+{
+ints_swapping(array + start, array + large);
+print_array(array, size);
+ConvertToHeap(array, size, end, large);
+}
 }
 
 /**
@@ -38,20 +38,17 @@ void ConvertToHeap(int *array, size_t size, size_t end, size_t start)
  */
 void heap_sort(int *array, size_t size)
 {
-	int i;
-
-	if (array == NULL || size < 2)
-		return;
-
-	for (i = (size / 2) - 1; i >= 0; i--)
-		ConvertToHeap(array, size, size, i);
-
-	for (i = size - 1; i > 0; i--)
-	{
-		ints_swapping(array, array + i);
-		print_array(array, size);
-		ConvertToHeap(array, size, i, 0);
-	}
+int i;
+if (array == NULL || size < 2)
+return;
+for (i = (size / 2) - 1; i >= 0; i--)
+ConvertToHeap(array, size, size, i);
+for (i = size - 1; i > 0; i--)
+{
+ints_swapping(array, array + i);
+print_array(array, size);
+ConvertToHeap(array, size, i, 0);
+}
 }
 /**
  * ints_swapping - this is the Swap between two int in an array.
@@ -60,8 +57,8 @@ void heap_sort(int *array, size_t size)
  */
 void ints_swapping(int *previous, int *current)
 {
-	int tmp;
-	tmp = *previous;
-	*previous = *current;
-	*current = tmp;
+int tmp;
+tmp = *previous;
+*previous = *current;
+*current = tmp;
 }
