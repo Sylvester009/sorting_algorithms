3c18f1f253ea8c62942fef1cba4d83570e6e2988
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-19 11:07:30 +0100

    Update 106-bitonic_sort.c

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,89 +1,92 @@
 #include "sort.h"
 #include <stdio.h>
-
+void bitoniComp(bool boolean, int *subarray, size_t size);
+void merge_bitonic(bool boolean, int *subarray, size_t size, size_t first_size);
+void bitonic_sort_r(bool boolean, int *subarray, size_t size, size_t first_size);
 /**
- * bitonic_compare - helper to bitonic_merge, sorts contents of current
+ * bitoniComp - this is a helper, and it sorts contents of current
  * subarray
- * @up: if true, sort in ascending order, false, descending
- * @x: subarray in current frame of recursion
+ * @boolean: if true, sort in ascending order, false, descending
+ * @subarray: subarray in current frame of recursion
  * @size: number of elements in `x`
  */
-void bitonic_compare(bool up, int *x, size_t size)
+void bitoniComp(bool boolean, int *subarray, size_t size)
 {
-	size_t dist, i;
-	int temp;
+size_t dist = size / 2;
+size_t index = 0;
 
-	dist = size / 2;
-	for (i = 0; i < dist; i++)
-	{
-		if ((x[i] > x[i + dist]) == up)
-		{
-			temp = x[i];
-			x[i] = x[i + dist];
-			x[i + dist] = temp;
-		}
-	}
+while (index < dist)
+{
+if ((subarray[index] > subarray[index + dist]) == boolean)
+{
+int temp = subarray[index];
+subarray[index] = subarray[index + dist];
+subarray[index + dist] = temp;
+}
+index++;
+}
 }
 
 /**
- * bitonic_merge - second recursive function of bitonic_sort, sorts subarrays
- * via bitonic_compare, and merges the sorted results
- * @up: if true, sort in ascending order, false, descending
- * @x: subarray in previous frame of recursion
- * @size: number of elements in `x`
- * @orig_size: number of elements in source array being sorted
+ * merge_bitonic - this is the second recursive function,that sorts subarrays
+ * through bitonic_compare, and merges
+ * @boolean: when a statement true, it sort in ascending order
+ * and when false descending order
+ * @subarray: this is the subarray in previous frame of recursion
+ * @size: the number of elements in 'x'(subarray)
+ * @first_size: the number of elements in source array being sorted
  */
-void bitonic_merge(bool up, int *x, size_t size, size_t orig_size)
+void merge_bitonic(bool boolean, int *subarray, size_t size, size_t first_size)
 {
-	int *first, *second;
+int *st, *nd;
 
-	if (size > 1)
-	{
-		first = x;
-		second = x + (size / 2);
-		bitonic_compare(up, x, size);
-		bitonic_merge(up, first, size / 2, orig_size);
-		bitonic_merge(up, second, size / 2, orig_size);
-	}
+while (size > 1)
+{
+st = subarray;
+nd = subarray + (size / 2);
+bitoniComp(boolean, subarray, size);
+merge_bitonic(boolean, st, size / 2, first_size);
+merge_bitonic(boolean, nd, size / 2, first_size);
+}
 }
 
 /**
- * bitonic_sort_r - first recursive engine of bitonic_sort, divides array
- * into a binary tree of subarrays, and assigns sorting order.
- * @up: if true, sort in ascending order, false, descending
- * @x: subarray in previous frame of recursion
- * @size: number of elements in `x`
- * @orig_size: number of elements in source array being sorted
+ * sortbitonic - this is the first recursive valve
+ * of bitonic_sort that is  divided into a binary tree of
+ * subarrays the  assigns sorting order.
+ * @boolean:  when a statement true, it sort in ascending
+ *  and when false descending order
+ * @subarray: this is the subarray in previous frame of recursion
+ * @size: the number of elements in 'x'(subarray)
+ * @first_size: the number of elements in source array being sorted
  */
-void bitonic_sort_r(bool up, int *x, size_t size, size_t orig_size)
+void sortbitonic(bool boolean, int *subarray, size_t size, size_t first_size)
 {
-	int *first, *second;
-
-	if (size <= 1)
-		return;
-	first = x;
-	second = x + (size / 2);
-	printf("Merging [%lu/%lu] (%s):\n", size, orig_size,
-	       (up ? "UP" : "DOWN"));
-	print_array(x, size);
-	bitonic_sort_r(true, first, size / 2, orig_size);
-	bitonic_sort_r(false, second, size / 2, orig_size);
-	bitonic_merge(up, first, size, orig_size);
-	printf("Result [%lu/%lu] (%s):\n", size, orig_size,
-	       (up ? "UP" : "DOWN"));
-	print_array(x, size);
+int *st, *nd;
+while (size <= 1)
+return;
+st = subarray;
+nd = subarray + (size / 2);
+printf("Merging [%lu/%lu] (%s):\n", size, first_size,
+(boolean ? "UP" : "DOWN"));
+print_array(subarray, size);
+sortbitonic(true, st, size / 2, first_size);
+sortbitonic(false, nd, size / 2, first_size);
+merge_bitonic(boolean, st, size, first_size);
+printf("Result [%lu/%lu] (%s):\n", size, first_size,
+(boolean ? "UP" : "DOWN"));
+print_array(subarray, size);
 }
 
 /**
- * bitonic_sort - sorts array of integers in ascending order using a bitonic
- * sort alogrithm
- * @array: array of values to be printed
- * @size: number of elements in array
+ * bitonic_sort - it sorts arrays of int type
+ * in ascending order using a bitonic sort alogrithm method
+ * @array: this is the array values to be printed
+ * @size: this is the number of elements in array
  */
 void bitonic_sort(int *array, size_t size)
 {
-	if (!array || size == 0)
-		return;
-
-	bitonic_sort_r(true, array, size, size);
+while (!array || size == 0)
+return;
+sortbitonic(true, array, size, size);
 }
