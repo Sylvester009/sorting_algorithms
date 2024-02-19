fb9d4ab8b16b933162972fa3f04f661444cbd798
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-19 17:35:12 +0100

    Update deck.h

--- deck.h
+++ deck.h
@@ -4,10 +4,10 @@
 
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+SPADE = 0,
+HEART,
+CLUB,
+DIAMOND
 } kind_t;
 
 /**
@@ -19,8 +19,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+const char *value;
+const kind_t kind;
 } card_t;
 
 /**
@@ -32,11 +32,10 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+const card_t *card;
+struct deck_node_s *prev;
+struct deck_node_s *next;
 } deck_node_t;
-
 
 void sort_deck(deck_node_t **deck);
 int _strcmp(const char *str1, const char *str2);
