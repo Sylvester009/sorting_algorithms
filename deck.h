a11fa879aed784645dfa8aa1f141fd6d10f53990
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-19 17:37:09 +0100

    Update deck.h

--- deck.h
+++ deck.h
@@ -1,6 +1,14 @@
 #ifndef DECK_H
 #define DECK_H
 
+/**
+ * enum kind_e -this will definitely Enumeration of
+ * card suits.
+ * @SPADE: this is the Spades suit.
+ * @HEART: this is the Hearts suit.
+ * @CLUB: this is the Clubs suit.
+ * @DIAMOND: this is the Diamonds suit.
+ */
 
 typedef enum kind_e
 {
