e286db8fc566ed0b03a91ee130a45c3684af1012
Samuel Sylvester <samsylvester09@gmail.com>
2024-02-20 10:55:47 +0100

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -3,80 +3,89 @@
 /**
  * swap_adjacent - it swaps two adjacent nodes
  * @list: doubly_linked list of int type to be sorted
- * @tail: node closer to head
- * @head: node closer to tail
+ * @tail: it is node closer to head
+ * @head: this is node closer to tail
  */
 void swap_adjacent(listint_t **list, listint_t *tail, listint_t *head)
 {
-    if (tail->prev != NULL)
-        tail->prev->next = head;
-    else
-        *list = head;
+	listint_t *swap;
 
-    if (head->next != NULL)
-        head->next->prev = tail;
+	if (tail->prev != NULL)
+	{
+		tail->prev->next = head;
+	}
+	else
+	{
+		*list = head;
+	}
 
-    head->prev = tail->prev;
-    tail->prev = head;
-    tail->next = head->next;
-    head->next = tail;
-    print_list(*list);
+	if (head->next != NULL)
+	{
+		head->next->prev = tail;
+	}
+
+	head->prev = tail->prev;
+	tail->prev = head;
+	swap = head;
+	tail->next = head->next;
+	swap->next = tail;
+	print_list(*list);
 }
 
 /**
- * cocktail_sort_list - sorts a doubly_linked list of int type in ascending order
- * @list: doubly_linked list of int type to be sorted
+ * cocktail_sort_list - it is sorts a doubly_linked list of int type
+ * in ascending order
+ * @list: doubly_linked list of intm type to be sorted
  */
 void cocktail_sort_list(listint_t **list)
 {
-    bool forwardSwapped, backwardSwapped;
-    int s_Range = 1000000;
+	bool fowardswapped, backwardSwapped;
+	int s_Range = 1000000, checks;
+	listint_t *temp;
 
-    if (!list || !(*list) || !(*list)->next)
-        return;
+	if (!list || !(*list) || !(*list)->next)
+		return;
 
-    listint_t *temp = *list;
+	temp = *list;
+	while (1)
+	{
+		fowardswapped = backwardSwapped = false;
+		for (checks = 0; temp->next && checks < s_Range; checks++)
+		{
+			if (temp->next->n < temp->n)
+			{
+				swap_adjacent(list, temp, temp->next);
+				fowardswapped = true;
+			}
+			else
+			{
+				temp = temp->next;
+			}
+		}
+		if (!temp->next)
+			s_Range = checks;
 
-    while (1) {
-        forwardSwapped = backwardSwapped = false;
+		if (fowardswapped)
+			temp = temp->prev;
 
-        for (int checks = 0; temp->next && checks < s_Range; checks++)
-        {
-            if (temp->next->n < temp->n)
-            {
-                swap_adjacent(list, temp, temp->next);
-                forwardSwapped = true;
-            }
-            else
-            {
-                temp = temp->next;
-            }
-        }
+		s_Range--;
 
-        if (!temp->next)
-            s_Range = checks;
+		for (checks = 0; temp->prev && checks < s_Range; checks++)
+		{
+			if (temp->n < temp->prev->n)
+			{
+				swap_adjacent(list, temp->prev, temp);
+				backwardSwapped = true;
+			}
+			else
+			{
+				temp = temp->prev;
+			}
+		}
+		if (backwardSwapped)
+			temp = temp->next;
 
-        if (forwardSwapped)
-            temp = temp->prev;
-
-        s_Range--;
-
-        for (int checks = 0; temp->prev && checks < s_Range; checks++)
-        {
-            if (temp->n < temp->prev->n) {
-                swap_adjacent(list, temp->prev, temp);
-                backwardSwapped = true;
-            }
-            else
-            {
-                temp = temp->prev;
-            }
-        }
-
-        if (backwardSwapped)
-            temp = temp->next;
-
-        if (!forwardSwapped && !backwardSwapped)
-            break;
-    }
+		if (!fowardswapped && !backwardSwapped)
+			break;
+	}
 }
