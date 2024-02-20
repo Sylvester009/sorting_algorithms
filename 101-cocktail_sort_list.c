776f429656cb131a6ca92583044d0a1fabb60cc5
Samuel Sylvester <samsylvester09@gmail.com>
2024-02-20 10:04:54 +0100

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -6,25 +6,28 @@
  * @tail: it is node closer to head
  * @head: this is node closer to tail
  */
-void swap_adjacent(listint_t **list, listint_t *tail, listint_t *head) {
-    listint_t *swap;
+void swap_adjacent(listint_t **list, listint_t *tail, listint_t *head)
+{
+listint_t *swap;
 
-    if (tail->prev != NULL) {
-        tail->prev->next = head;
-    } else {
-        *list = head;
-    }
+if (tail->prev != NULL) {
+tail->prev->next = head;
+}
+else
+{
+*list = head;
+}
 
-    if (head->next != NULL) {
-        head->next->prev = tail;
-    }
+if (head->next != NULL) {
+head->next->prev = tail;
+}
 
-    head->prev = tail->prev;
-    tail->prev = head;
-    swap = head;
-    tail->next = head->next;
-    swap->next = tail;
-    print_list(*list);
+head->prev = tail->prev;
+tail->prev = head;
+swap = head;
+tail->next = head->next;
+swap->next = tail;
+print_list(*list);
 }
 
 /**
@@ -35,17 +38,21 @@ void swap_adjacent(listint_t **list, listint_t *tail, listint_t *head) {
  * @s_Range: the range of elements to check
  * @return: pointer to the current node after the forward pass
  */
-listint_t* forward_pass(listint_t **list, listint_t *temp, bool *forwardSwapped, int s_Range) {
-    int checks;
-    for (checks = 0; temp->next && checks < s_Range; checks++) {
-        if (temp->next->n < temp->n) {
-            swap_adjacent(list, temp, temp->next);
-            *forwardSwapped = true;
-        } else {
-            temp = temp->next;
-        }
-    }
-    return temp;
+listint_t* forward_pass(listint_t **list, listint_t *temp, bool *forwardSwapped, int s_Range)
+{
+int checks;
+for (checks = 0; temp->next && checks < s_Range; checks++) {
+if (temp->next->n < temp->n)
+{
+swap_adjacent(list, temp, temp->next);
+*forwardSwapped = true;
+}
+else
+{
+temp = temp->next;
+}
+}
+return (temp);
 }
 
 /**
@@ -56,17 +63,19 @@ listint_t* forward_pass(listint_t **list, listint_t *temp, bool *forwardSwapped,
  * @s_Range: the range of elements to check
  * @return: pointer to the current node after the backward pass
  */
-listint_t* backward_pass(listint_t **list, listint_t *temp, bool *backwardSwapped, int s_Range) {
-    int checks;
-    for (checks = 0; temp->prev && checks < s_Range; checks++) {
-        if (temp->n < temp->prev->n) {
-            swap_adjacent(list, temp->prev, temp);
-            *backwardSwapped = true;
-        } else {
-            temp = temp->prev;
-        }
-    }
-    return temp;
+listint_t* backward_pass(listint_t **list, listint_t *temp, bool *backwardSwapped, int s_Range)
+{
+int checks;
+for (checks = 0; temp->prev && checks < s_Range; checks++)
+{
+if (temp->n < temp->prev->n) {
+swap_adjacent(list, temp->prev, temp);
+*backwardSwapped = true;
+} else {
+temp = temp->prev;
+}
+}
+return (temp);
 }
 
 /**
@@ -74,25 +83,26 @@ listint_t* backward_pass(listint_t **list, listint_t *temp, bool *backwardSwappe
  * in ascending order
  * @list: doubly_linked list of int type to be sorted
  */
-void cocktail_sort_list(listint_t **list) {
-    if (!list || !(*list) || !(*list)->next)
-        return;
+void cocktail_sort_list(listint_t **list)
+{
+int s_Range = 1000000;
+listint_t *temp;
+bool forwardSwapped, backwardSwapped;
+if (!list || !(*list) || !(*list)->next)
+return;
+temp = *list;
+while (1)
+{
+forwardSwapped = backwardSwapped = false;
 
-    int s_Range = 1000000;
-    listint_t *temp = *list;
-    bool forwardSwapped, backwardSwapped;
+temp = forward_pass(list, temp, &forwardSwapped, s_Range);
 
-    while (1) {
-        forwardSwapped = backwardSwapped = false;
+if (!temp->next)
+s_Range = count_remaining_nodes(temp, s_Range);
 
-        temp = forward_pass(list, temp, &forwardSwapped, s_Range);
+temp = backward_pass(list, temp, &backwardSwapped, s_Range);
 
-        if (!temp->next)
-            s_Range = count_remaining_nodes(temp, s_Range);
-
-        temp = backward_pass(list, temp, &backwardSwapped, s_Range);
-
-        if (!forwardSwapped && !backwardSwapped)
-            break;
-    }
+if (!forwardSwapped && !backwardSwapped)
+break;
+}
 }
