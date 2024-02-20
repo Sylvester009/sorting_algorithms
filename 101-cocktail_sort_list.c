1189f3c613bbb9550d88a61856b0364960d8fc26
Samuel Sylvester <samsylvester09@gmail.com>
2024-02-20 10:24:42 +0100

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -34,66 +34,49 @@ print_list(*list);
  * in ascending order
  * @list: doubly_linked list of intm type to be sorted
  */
-void cocktail_sort_list(listint_t **list) {
-    if (!list || !(*list) || !(*list)->next)
-        return;
-
-    bool forwardSwapped, backwardSwapped;
-    int s_Range = 1000000;
-    listint_t *temp = *list;
-
-    while (1) {
-        forwardSwapped = backwardSwapped = false;
-
-        // Forward pass
-        temp = forward_pass(list, temp, &forwardSwapped, &s_Range);
-
-        // Adjust range if needed
-        if (!temp->next)
-            s_Range = get_remaining_range(temp, s_Range);
-
-        // Backward pass
-        temp = backward_pass(list, temp, &backwardSwapped, &s_Range);
-
-        // Check if no swaps occurred, break if so
-        if (!forwardSwapped && !backwardSwapped)
-            break;
-    }
+void cocktail_sort_list(listint_t **list)
+{
+bool fowardswapped, backwardSwapped;
+int s_Range = 1000000, checks;
+listint_t *temp;
+if (!list || !(*list) || !(*list)->next)
+return;
+temp = *list;
+while (1)
+{
+fowardswapped = backwardSwapped = false;
+for (checks = 0; temp->next && checks < s_Range; checks++)
+{
+if (temp->next->n < temp->n)
+{
+swap_adjacent(list, temp, temp->next);
+fowardswapped = true;
 }
-
-listint_t* forward_pass(listint_t **list, listint_t *temp, bool *forwardSwapped, int *s_Range) {
-    int checks;
-
-    for (checks = 0; temp->next && checks < *s_Range; checks++) {
-        if (temp->next->n < temp->n) {
-            swap_adjacent(list, temp, temp->next);
-            *forwardSwapped = true;
-        } else {
-            temp = temp->next;
-        }
-    }
-    return temp;
+else
+{
+temp = temp->next;
 }
-
-listint_t* backward_pass(listint_t **list, listint_t *temp, bool *backwardSwapped, int *s_Range) {
-    int checks;
-
-    for (checks = 0; temp->prev && checks < *s_Range; checks++) {
-        if (temp->n < temp->prev->n) {
-            swap_adjacent(list, temp->prev, temp);
-            *backwardSwapped = true;
-        } else {
-            temp = temp->prev;
-        }
-    }
-    return temp;
 }
-
-int get_remaining_range(listint_t *temp, int s_Range) {
-    int count = 0;
-    while (temp->next && count < s_Range) {
-        temp = temp->next;
-        count++;
-    }
-    return count;
+if (!temp->next)
+s_Range = checks;
+if (fowardswapped)
+temp = temp->prev;
+s_Range--;
+for (checks = 0; temp->prev && checks < s_Range; checks++)
+{
+if (temp->n < temp->prev->n)
+{
+swap_adjacent(list, temp->prev, temp);
+backwardSwapped = true;
+}
+else
+{
+temp = temp->prev;
+}
+}
+if (backwardSwapped)
+temp = temp->next;
+if (!fowardswapped && !backwardSwapped)
+break;
+}
 }
